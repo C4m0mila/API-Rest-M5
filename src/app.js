f7b2779e462212c07c0553e760b7d68a46b2c867
C4m0mila <112404881+C4m0mila@users.noreply.github.com>
2023-03-05 21:25:04 -0300

    Criando os métodos GET/POST/PUT/DELETE
    Criei 5 colunas na tabela.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // import {openDb} from './configDB.js';
-import { createTable, insertPonto } from './controller/controlePonto.js'
+import { createTable, insertPonto, updatePonto, selectAllPontos, selectPonto, deletePonto } from './controller/controlePonto.js'
 
 import express from 'express';
 const app = express();
@@ -8,14 +8,44 @@ app.use(express.json());
 createTable();
 
 app.get('/', function (req, res){
-    res.send('Hello World');
+    res.send('Api rodando');
 });
 
-app.post('/controlePonto', function (req, res){
+app.get('/Pontos',  async function (req, res){
+let Pontos = await selectAllPontos();
+res.json(Pontos)
+});
+
+app.get('/Ponto',  async function (req, res){
+    let Ponto = await selectPonto(req.body.id);
+    res.json(Ponto)
+    });
+
+app.post('/Ponto', function (req, res){
     insertPonto(req.body)
     res.json({
         "statusCode": 200
     })
 })
 
+app.put('/Ponto', function (req, res){
+    if(req.body && !req.body.id){
+        res.json({
+            "statusCode": "400",
+        "msg": "Você precisa informar um id"
+        })
+    } else{
+        updatePonto(req.body)
+        res.json({
+            "statusCode": 200
+        })
+    }
+})
+
+app.delete('/Ponto',  async function (req, res){
+    let Ponto = await deletePonto();
+    res.json(Ponto) 
+    });
+
 app.listen(3000, () => console.log("Api rodando."));
+
--- src/controller/controlePonto.js
+++ src/controller/controlePonto.js
@@ -3,11 +3,57 @@ import {openDb} from '../configDB.js';
 export async function createTable(){
     openDb().then(db=>{
         db.exec('CREATE TABLE IF NOT EXISTS Ponto ( id INTEGER PRIMARY KEY AUTOINCREMENT, funcionario TEXT, dia DATE NOT NULL, entrada TIME DEFAULT NULL, saida TIME DEFAULT NULL)')
-    })
+        console.log('Tabela criada com sucesso');
+    }).catch((error) => {
+      console.error(error);    
+    });
 }
 
 export async function insertPonto(controlePonto){
     openDb().then(db=>{
         db.run('INSERT INTO Ponto (funcionario, dia, entrada, saida) VALUES (?,?,?,?)', [controlePonto.funcionario, controlePonto.dia, controlePonto.entrada, controlePonto.saida]);
+    }).then(() => {
+        console.log("Registro inserido com sucesso!");
+    }).catch((err) => {
+        console.log("Erro ao inserir registro: ", err);
     });
 }
+
+export async function updatePonto(controlePonto){
+    openDb().then(db=>{
+        db.run('UPDATE Ponto SET funcionario=?, dia=?, entrada=?, saida=? WHERE id=?', [controlePonto.funcionario, controlePonto.dia, controlePonto.entrada, controlePonto.saida, controlePonto.id]);
+    }) .then(() => {
+            console.log('Registro atualizado com sucesso');
+            return true;
+          })
+          .catch(err => {
+            console.error(err);
+            return false;
+          });
+}
+
+
+export async function selectAllPontos(){
+   return openDb().then(db=>{
+       return db.all('SELECT * from Ponto')
+        .then(res=>res)
+    });
+}
+
+export async function selectPonto(id){
+  return openDb().then(db => {
+    return db.get('SELECT * from Ponto WHERE id=?', [id])
+      .then(res => res)
+      .catch(err => {
+        console.error(err);
+        throw err;
+      });
+  });
+}
+
+export async function deletePonto(id){
+    return openDb().then(db => {
+      return db.get('DELETE from Ponto WHERE id=?', [id])
+        });
+  }
+
